ac45f1108ab155b7f62b9bb9a0256cffa923804b
MeanbyMin <soeg0810@gmail.com>
2021-06-27 12:27:26 +0900

    fetch에서 axios으로 바꿔서 사용하기

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import Youtube from './service/youtube';
+import axios from 'axios';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+// axios를 사용할 때
+const httpClient = axios.create({
+  baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+const youtube = new Youtube(httpClient);
+
+// process 환경변수를 사용할때
+// const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
